bf6f1d1a41624593723b691aadab4f05bab0506f
wuzhiqiang <1452366827@qq.com>
2024-02-04 15:04:24 +0800

    wip: 完成日期组件布局

---
+++ src/Calendar/index.tsx
@@ -0,0 +1,15 @@
+import { Dayjs } from 'dayjs';
+import MonthCalendar from './MonthCalendar';
+import './index.scss';
+
+export interface CalendarProps {
+  value: Dayjs
+}
+
+function Calendar(props: CalendarProps) {
+    return <div className="calendar">
+        <MonthCalendar {...props} />
+    </div>
+}
+
+export default Calendar;
